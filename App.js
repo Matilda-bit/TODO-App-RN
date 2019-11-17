aac004d36893551893b05173bf47e3ff9452f72d
Matilda-bit <polinaovras@gmail.com>
2019-11-17 12:55:14 +0200

    check

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header';
 import InputBar from './components/InputBar';
-import  from './components/TodoItem';
+import TodoItem from './components/TodoItem';
 
 export default class App extends React.Component {
   constructor(){
