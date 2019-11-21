1d4c3f2c71c2909095a7038b0b109c4a4955e8ad
Matilda-bit <polinaovras@gmail.com>
2019-11-21 21:35:38 +0200

    add main button

---
+++ components/Buttons/MainButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, TouchableOpacity,StyleSheet} from 'react-native';
+
+import Colors from '../../constants/colors'
+const StartButton = (props) => {
+    return (
+        <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>{ props.children }</Text>
+            </View>
+        </TouchableOpacity>
+        
+    );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        width: 30,
+        height: 20,
+        borderRadius: 3,
+        borderWidth: 2,
+        borderColor: Colors.my_black, 
+        backgroundColor: Colors.basic_menta
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: Colors.my_white,
+        fontFamily: 'my-font-2',
+        fontSize: 18
+    }
+});
+
+export default StartButton;
