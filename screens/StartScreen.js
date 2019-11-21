f9ed96db8aab0a95940d6d6477ae788a5e76243f
Matilda-bit <polinaovras@gmail.com>
2019-11-21 21:20:39 +0200

    add Screen Start

---
+++ screens/StartScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import Colors from '../constants/colors';
+import StartButton from '../components/Buttons/StartButton';
+
+useEffect(() => {
+    if (currentGuess === userChoice){
+        onGameOver(pastGuesses.length);
+    }
+
+const StartScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <View style={styles.container}>
+                {/* <Button title='Start' style={styles.buttonContainer} /> */}
+                <StartButton onPress={() => props.onStart()}>
+                    I'm Ready
+                </StartButton>
+            </View>
+            
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 100,
+        alignItems: 'center'
+    },
+    conainer:{
+        width: '100%',
+        height: '100%',
+    },
+    buttonContainer: {
+       
+    }
+});
+
+export default StartScreen;
