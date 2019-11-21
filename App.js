a2f67f73696da88af012cfefa27360e609420a27
Matilda-bit <polinaovras@gmail.com>
2019-11-21 21:21:33 +0200

    add content StartScreen

--- App.js
+++ App.js
@@ -1,106 +1,46 @@
-import React from 'react';
-import { Platform, StyleSheet, Text, View, FlatList } from 'react-native';
-import Header from './components/Header';
-import InputBar from './components/InputBar';
-import TodoItem from './components/TodoItem';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 
-export default class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-        todoInput: '',
-        todos: [
-          { id: 0, title: 'Take out the trash', done: false , textlen: 18 }
-        ]
-    }
-  }
+import StartScreen from './screens/StartScreen';
 
-  addNewTodo() {
-    let todos = this.state.todos;
-    console.log(this.state.todoInput)
-    if(this.state.todoInput.length != 0){
-      todos.unshift({
-        id: todos.length,
-        title: this.state.todoInput,
-        done: false,
-        textlen: this.state.todoInput.length
-  
-      });
-  
-      this.setState({
-        todos,
-        todoInput: ''
-      });
-      
-    }
-    }
-    
+const fetchFonts = () => {
+  return Font.loadAsync({
+      'my-font-1': require('./assets/fonts/Amatic-Bold.ttf'),
+      'my-font-2': require('./assets/fonts/AmaticSC-Regular.ttf'),
+      'my-font-3': require('./assets/fonts/Lato-Bold.ttf')
+  });
+};
 
-  toggleDone (item){
-    let todos = this.state.todos;
-    
-    todos = todos.map((todo) => {
-      if (todo.id === item.id) {
-        todo.done = !todo.done;
-      }
-      
-      return todo;
-    })
-    this.setState({todos});
-  }
+export default function App() {
+  const [dataLoaded, setdataLoaded] = useState(false);
 
-  removeTodo (item){
-    let todos = this.state.todos;
-    todos = todos.filter((todo) => todo.id != item.id)
+  if(!dataLoaded){
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => setdataLoaded(true)} 
+        onError={(err) => err} 
+      />
+    );
+  };
 
-    this.setState({todos});
-  }
+  let content = <StartScreen />;
 
- 
-render(){
-  const statusbar = (Platform.OS == 'android') ? <View style={styles.statusbar}></View> : <View></View>;
-
-  return (
-    <View style={styles.container}>
-        {statusbar}
-
-        <Header title="TODO APP"/>
-
-        <InputBar 
-            addNewTodo={ () => this.addNewTodo()}
-            textChange={todoInput => this.setState({ todoInput })} 
-            todoInput={this.state.todoInput}
-            
-          />
-        
-          <FlatList 
-                  data={ this.state.todos}
-                  extraData={this.state}
-                  keyExtractor={(item, index) => index.toString()}
-                  renderItem={ ({item, index}) => {
-                    return(
-                      <TodoItem 
-                        todoItem={item} toggleDone={() => this.toggleDone(item)}
-                        removeTodo={() => this.removeTodo(item)}
-                      />
-                    )
-                  } }
-            />
-          
-                
-        </View>
+  if (userChoice) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+      );
+  return(
+    <View style={styles.screen}>
+      {content}
+    </View>
   );
-}
-}
-
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-  statusbar: { 
-    backgroundColor: '#FFCE00',
-    height: 24
+  screen: {
+    flex: 1
   }
 });
