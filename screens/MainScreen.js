2770c1ad87ae144d0a04e9e05794b29a90d698de
Matilda-bit <polinaovras@gmail.com>
2019-11-21 21:36:13 +0200

    add main screen

---
+++ screens/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const MainScreen = props => {
+    return (
+        <View style={styles.screen}></View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {}
+});
