9de660461ccc4930e136fc5b92f554267d9aa201
Matilda-bit <polinaovras@gmail.com>
2019-11-22 23:59:16 +0200

    add StartScreen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import Colors from '../constants/colors';
 import StartButton from '../components/Buttons/StartButton';
+import BodyText from '../components/Text/BodyText'
 
 
-const StartScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <View style={styles.container}>
-                {/* <Button title='Start' style={styles.buttonContainer} /> */}
-                <StartButton onPress={() => props.onStart()}>
-                    I'm Ready
+
+const StartScreen  = props => {
+
+    const [roundsClick, setRoundsClick] = useState(0);
+    const { onMain } = props;
+    
+    useEffect(() => {
+        if (roundsClick > 2){
+            onMain();
+        }
+    }, [roundsClick, onMain]); 
+    
+
+    onPressHandler = () => {
+    setRoundsClick(roundsClick +1)
+    }
+    
+        return (
+            <View style={styles.screen}>
+
+                {/* button  */}
+                <View style={styles.container}>
+
+                <Text style={[styles.countText]}>
+                        { this.roundsClick !== 0 ? this.roundsClick: null}
+                </Text>
+                <StartButton  onPressHandler={this.onPressHandler} >
+                    <Text style={styles.boxButton}></Text>
                 </StartButton>
+
+                </View>
+
+                {/* text */}
+                <View>
+                    <BodyText style={styles.box}>
+                        no, {"\n"}
+                        I'm not Ready...
+                    </BodyText>
+                </View>  
             </View>
-            
-        </View>
     );
-};
+}
+
 
 const styles = StyleSheet.create({
     screen: {
@@ -29,7 +60,27 @@ const styles = StyleSheet.create({
     conainer:{
         width: '100%',
         height: '100%',
-    }
+    },
+    highlight: {
+        color: Colors.primary,
+    },
+    line: {
+        textDecorationLine: 'line-through'
+    },
+    box: {
+        paddingTop: 20,
+        textAlign: 'center'
+    },
+    boxButton: {
+        height: 10
+    },
+    countContainer: {
+        alignItems: 'center',
+        padding: 10
+      },
+    countText: {
+        color: '#FF00FF'
+      }
 });
 
 export default StartScreen;
