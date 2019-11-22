26b25735b9d2bdeb997382b7d53d884f9d07cac2
Matilda-bit <polinaovras@gmail.com>
2019-11-23 00:00:16 +0200

    add Main Screen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Ionicons } from 'expo/vector-icon';
+import { View, StyleSheet, FlatList } from 'react-native';
+// import { Ionicons } from 'expo/vector-icon';
 
+import Header from '../components/Header';
+import InputBar from '../components/InputBar';
+import Colors from '../constants/colors';
 
 
 const MainScreen = props => {
-    return (
-        <View style={styles.screen}></View>
-    );
+        return (
+            <View style={styles.container}>
+                <View style={styles.statusbar}></View>
+                {/* <Header title= "TODO APP" /> */}
+
+            </View>
+        );  
 };
 
 const styles = StyleSheet.create({
-    screen: {}
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    statusbar: { 
+        backgroundColor: Colors.basic_menta,
+        height: 24
+      }
 });
+
+export default MainScreen;
