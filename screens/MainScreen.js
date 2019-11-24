035dab500ebf012d2e8f870c0b1dff04fefe90e6
Matilda-bit <polinaovras@gmail.com>
2019-11-24 23:26:27 +0200

    add Main Screen TODO list

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -4,18 +4,89 @@ import { View, StyleSheet, FlatList } from 'react-native';
 
 import Header from '../components/Header';
 import InputBar from '../components/InputBar';
+import TodoItem from '../components/TodoItem';
 import Colors from '../constants/colors';
 
 
-const MainScreen = props => {
+export default class MainScreen extends React.Component {
     
+    constructor(){
+        super()
+        this.state = {
+            todoInput: '', 
+            todos: [
+              { id: 0, title: 'Take out the trash', done: false , textlen: 18 }
+            ]
+        }
+      }
+
+    addNewTodo() {
+        let todos = this.state.todos;
+        console.log(this.state.todoInput)
+        if(this.state.todoInput.length != 0){
+          todos.unshift({
+            id: todos.length,
+            title: this.state.todoInput,
+            done: false,
+            textlen: this.state.todoInput.length
+      
+          });
+          this.setState({
+            todos,
+            todoInput: ''
+          }); 
+        }
+    }
+
+    toggleDone (item){
+        let todos = this.state.todos;
+        
+        todos = todos.map((todo) => {
+            if (todo.id === item.id) {
+                todo.done = !todo.done;
+            }
+          
+             return todo;
+        })
+        this.setState({todos});
+    }
+
+    removeTodo (item){
+        let todos = this.state.todos;
+        todos = todos.filter((todo) => todo.id != item.id)
+    
+        this.setState({todos});
+    }
+
+    render () {
         return (
             <View style={styles.container}>
                 <View style={styles.statusbar}></View>
-                {/* <Header title= "TODO APP" /> */}
-
+    
+                 <Header title= "TODO APP" />
+    
+                 <InputBar 
+                    addNewTodo={ () => this.addNewTodo()}
+                    textChange={todoInput => this.setState({ todoInput })} 
+                    todoInput={this.state.todoInput}
+                />
+    
+                <FlatList 
+                    data={ this.state.todos}
+                    extraData={this.state}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={ ({item, index}) => {
+                        return(
+                        <TodoItem 
+                            todoItem={item} toggleDone={() => this.toggleDone(item)}
+                            removeTodo={() => this.removeTodo(item)}
+                         />
+                        )} 
+                    }
+                />
             </View>
-        );  
+        )
+    }        
 };
 
 const styles = StyleSheet.create({
@@ -28,5 +99,3 @@ const styles = StyleSheet.create({
         height: 24
       }
 });
-
-export default MainScreen;
