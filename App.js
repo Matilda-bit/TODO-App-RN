eecd71eee92b7825066fcb0cbd83b32da5b8afff
Amelie <31770058+Matilda-bit@users.noreply.github.com>
2021-01-04 17:02:35 +0200

    Update App.js
    update App.js

--- App.js
+++ App.js
@@ -1,32 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 
 import StartScreen from './screens/StartScreen';
 import MainScreen from './screens/MainScreen';
 
-
 const fetchFonts = () => {
   return Font.loadAsync({
-      'my-font-1': require('./assets/fonts/Amatic-Bold.ttf'),
-      'my-font-2': require('./assets/fonts/FFF.ttf'),
-      'my-font-3': require('./assets/fonts/Lato-Bold.ttf')
+    'my-font-1': require('./assets/fonts/Amatic-Bold.ttf'),
+    'my-font-2': require('./assets/fonts/FFF.ttf'),
+    'my-font-3': require('./assets/fonts/Lato-Bold.ttf')
   });
 };
 
 export default function App() {
-  const [dataLoaded, setdataLoaded] = useState(false);
-  const [userChoice, setUserChoice] = useState();
-  
+  // return (
+  //   // <View style={styles.container}>
+  //   //   <Text>Open up App.js to start working on your app!</Text>
+  //   //   <StatusBar style="auto" />
+  //   // </View>
+  // );
+
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [userChoice, setUserChoice] = useState(false);
 
 
-  if(!dataLoaded){
+
+  if (!dataLoaded) {
     return (
-      <AppLoading 
-        startAsync={fetchFonts} 
-        onFinish={() => setdataLoaded(true)} 
-        onError={(err) => err} 
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => err}
       />
     );
   };
@@ -35,24 +42,26 @@ export default function App() {
     setUserChoice(userChoice);
   }
 
-  let content = <StartScreen onStart={onStartHandler} />;
+  let content = <StartScreen choice={userChoice} onPress={onStartHandler} />;
 
-   if ( userChoice ) {
-     content = (
-      
-       <MainScreen />
-       );
-     };
+  if (userChoice) {
+    content = (
+      <MainScreen />
+    );
+  };
 
-  return(
-    <View style={styles.screen}>
+  return (
+    <View style={styles.container}>
       {content}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
