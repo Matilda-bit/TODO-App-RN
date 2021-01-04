53fa8acdd2a5bb360ea9863d95e6b7f6dea44d46
Amelie <31770058+Matilda-bit@users.noreply.github.com>
2021-01-04 17:05:08 +0200

    Update StartScreen.js
    update StartScreen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,101 +1,95 @@
 import React, { useState, useRef, useEffect } from 'react';
 import {
-    View, 
-    Text, 
-    StyleSheet, 
-    TouchableOpacity, 
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
     Alert
 } from 'react-native';
 
-import { Ionicons } from '@expo/vector-icons';
+// import { Ionicons } from '@expo/vector-icons';
 
 import Colors from '../constants/colors';
 import BodyText from '../components/Text/BodyText'
 
 
 
-const StartScreen  = (props) => {
-    const textOutputFirst = <Text>I'm Ready</Text>;
-    const textOutputSecond = <Text>Are You Sure?</Text>;
-    const textOutputLast = <Text>OKAY, let's do it!</Text>;
+const StartScreen = (props) => {
+    const output = ["I\'m Ready", "Are You Sure?", "OKAY, let's do it!"];
 
-    const textBotomOutput = <Text>no, {"\n"} I'm not Ready... {roundsClick}</Text>
-    const textBotomOutputFin =  <Text>
-                                    <Text style={styles.line}>no</Text>, {"\n"}{' '}
-                                    <Text style={styles.highlight}>I'm</Text> {' '} 
-                                    <Text style={styles.line}>not</Text> {' '} 
-                                    <Text style={styles.highlight}>Ready</Text>
+    const textBotomOutput = <Text>No. {"\n"} I'm not Ready...</Text>
+    const textBotomOutputFin = <Text>
+        <Text style={styles.line}>No</Text>.{"\n"}{' '}
+        <Text style={styles.highlight}>I'm</Text> {' '}
+        <Text style={styles.line}>not</Text> {' '}
+        <Text style={styles.highlight}>Ready</Text>
                                     ...
-                                </Text>; 
+                                </Text>;
 
-    const [ myText, setMyText ] = useState(textBotomOutput);
-    const [textOutput, setTextOutput] = useState(textOutputFirst);
+    const [myText, setMyText] = useState(textBotomOutput);
+    const [textOutput, setTextOutput] = useState(output[0]);
 
     const [roundsClick, setRoundsClick] = useState(0);
-    const [userChoice, setUserChoice] = useState(props.userChoice);
-    
-    
+    const [userChoice, setUserChoice] = useState(props.choice);
+
+
 
     const resetInputHandler = () => {
         setRoundsClick(0);
-        setTextOutput(textOutputFirst);
+        setTextOutput(output[0]);
         setMyText(textBotomOutput);
         //setResetChoice(true);
-    };   
+    };
 
     const noReadyHandler = () => {
-        if (!userChoice) {            
+        if (!userChoice) {
             Alert.alert('NOT READY?', 'Well, come back later...', [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }])
         };
-        
-        
     };
-    
-    onPress = () => {
-        setRoundsClick(roundsClick +1);
 
-        if 
-        (roundsClick === -1) {
-            setTextOutput(textOutputFirst);
+    const onPress = () => {
+        setRoundsClick(roundsClick + 1);
+
+        if
+            (roundsClick === -1) {
+            setTextOutput(output[0]);
         }
-        else if 
+        else if
             (roundsClick === 0) {
-                setTextOutput(textOutputSecond);  
+            setTextOutput(output[1]);
         }
-        else if 
+        else if
             (roundsClick === 1) {
-               setTextOutput(textOutputLast);
-               setMyText(textBotomOutputFin);
+            setTextOutput(output[2]);
+            setMyText(textBotomOutputFin);
         }
-
-        else if (roundsClick > 1){
-            props.onStart(true);
+        else if (roundsClick > 1) {
+            props.onPress(true);
         }
     }
-    
-        return (
-            <View style={styles.screen}>
 
-                {/* button  */}
-                <View style={styles.container}>
-              
-                    <TouchableOpacity activeOpacity={0.6} onPress={this.onPress}>
-                        <View style={styles.button}>
-                            <Text style={styles.buttonText}>{ textOutput }</Text>
-                        </View>
-                    </TouchableOpacity>
+    return (
+        <View style={styles.screen}>
 
-                </View>
+            {/* button  */}
+            <View style={styles.container}>
+                <TouchableOpacity activeOpacity={0.6} onPress={onPress}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>{textOutput}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
 
-                {/* text */}
-                <View>
-                    <TouchableOpacity activeOpacity={0.6} onPress = {noReadyHandler} >
+            {/* text   */}
+            <View>
+                <TouchableOpacity activeOpacity={0.6} onPress={noReadyHandler} >
                     <BodyText style={styles.box}>
                         {myText}
                     </BodyText>
-                    </TouchableOpacity>
-                </View>  
+                </TouchableOpacity>
             </View>
+
+        </View>
     );
 }
 
@@ -103,10 +97,10 @@ const StartScreen  = (props) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 100,
+        padding: 150,
         alignItems: 'center'
     },
-    conainer:{
+    conainer: {
         width: '100%',
         height: '100%',
     },
@@ -118,24 +112,24 @@ const styles = StyleSheet.create({
     },
     box: {
         paddingTop: 20,
+        width: '100%',
         textAlign: 'center'
     },
     countContainer: {
         alignItems: 'center',
         padding: 10
-      },
+    },
     countText: {
         color: '#FF00FF'
-      },
-      button: {
+    },
+    button: {
         width: 260,
         height: 260,
         borderRadius: 135,
         borderWidth: 3,
-        borderColor: Colors.my_black, 
+        borderColor: Colors.my_black,
         backgroundColor: Colors.basic_menta,
         paddingTop: 14
-       
     },
     buttonText: {
         //height: 10,
@@ -145,7 +139,7 @@ const styles = StyleSheet.create({
         color: Colors.my_white,
         fontFamily: 'my-font-1',
         fontSize: 60,
-        
+
         textAlignVertical: 'top'
     }
 });
